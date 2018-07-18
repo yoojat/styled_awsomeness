0c21761d918f3cf15335b5cfd67205e8a461c60a
yoojat <yooja.ty@gmail.com>
2018-07-18 10:04:40 +0900

    Theming

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component, Fragment } from "react";
-import styled, { injectGlobal, css } from "styled-components";
+import styled, { injectGlobal, css, ThemeProvider } from "styled-components";
+import theme from "./theme";
 
 injectGlobal`
 body{
@@ -8,12 +9,12 @@ body{
 }
 `;
 
-const awesomeCard = css`
-  box-shadow: 0 4px 6px rgba(50, 50, 93, 0.11), 0 1px 3px rgba(0, 0, 0, 0.08);
-  background-color: white;
-  border-radius: 10px;
-  padding: 20px;
-`;
+// const awesomeCard = css`
+//   box-shadow: 0 4px 6px rgba(50, 50, 93, 0.11), 0 1px 3px rgba(0, 0, 0, 0.08);
+//   background-color: white;
+//   border-radius: 10px;
+//   padding: 20px;
+// `;
 
 const Container = styled.div`
   height: 100vh;
@@ -21,20 +22,38 @@ const Container = styled.div`
   background-color: pink;
 `;
 
-const Input = styled.input.attrs({ required: true })`
-  border-radius: 5px;
-  border: none;
-  ${awesomeCard};
+// const Input = styled.input.attrs({ required: true })`
+//   border-radius: 5px;
+//   border: none;
+//   ${awesomeCard};
+// `;
+
+const Card = styled.div`
+  background-color: red;
+`;
+
+const Button = styled.button`
+  border-radius: 30px;
+  padding: 25px 15px;
+  background-color: ${props => props.theme.successColor};
 `;
 
 class App extends Component {
   render() {
     return (
-      <Container>
-        <Input placeholder="hello" />
-      </Container>
+      <ThemeProvider theme={theme}>
+        <Container>
+          <Form />
+        </Container>
+      </ThemeProvider>
     );
   }
 }
+
+const Form = () => (
+  <Card>
+    <Button>Hello</Button>
+  </Card>
+);
 
 export default App;
---
+++ src/theme.js
@@ -0,0 +1,7 @@
+const theme = {
+  mainColor: "#3498DB",
+  dangerColor: "#E74C3C",
+  successColor: "#2ECC71"
+};
+
+export default theme;
